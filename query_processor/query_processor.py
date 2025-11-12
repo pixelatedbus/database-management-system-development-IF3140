96c4fa37ec50e09ff8e05521bad1f4130e589f7d
Sanesasaha <13523066@std.stei.itb.ac.id>
2025-11-13 03:07:06 +0700

    feat: generating query tree on processor

--- query_processor/query_processor.py
+++ query_processor/query_processor.py
@@ -1,5 +1,10 @@
-from misc.storage import DataRetrieval, DataWrite, DataDeletion, Condition
-from misc.optimizer import QueryTree, ParsedQuery
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from misc import *
+from query_optimizer import *
 
 class QueryProcessor:
     _instance = None
@@ -11,3 +16,35 @@ class QueryProcessor:
 
     def __init__(self):
         pass
+
+    def execute_query(self, query:str):
+        try:
+            self._get_query_tree(query)
+        except Exception as e:
+            print(e) # could be more sophisticated...
+        
+        # TODO: continue
+        ...
+    
+    # ---------------------------- private methods ----------------------------
+    def _get_query_tree(self, query):
+        tok = Tokenizer(query)
+        parser = Parser(tok)
+
+        query_tree = parser.parse() # query tree is generated
+        check_query(query_tree) # query tree validation
+
+        # for testing purposes
+        print(query_tree)
+        print(query_tree.tree())
+
+        return query_tree
+
+# Unit Testing
+def main():
+    print("Hello, World")
+    processor = QueryProcessor()
+    processor.execute_query("SELECT * FROM users WHERE name='mifune';")
+
+if __name__ == "__main__":
+    main()
