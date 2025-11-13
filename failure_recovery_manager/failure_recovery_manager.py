87fb8d040f7e1b0f72112af8c17fbffe6f5f7e43
19623248Git <13523032@std.stei.itb.ac.id>
2025-11-13 21:14:30 +0700

    feat: :sparkles: import libraries

--- failure_recovery_manager/failure_recovery_manager.py
+++ failure_recovery_manager/failure_recovery_manager.py
@@ -1,11 +1,16 @@
+from buffer import buffer, table
+from log import actiontype
+from recovery_criteria import RecoveryCriteria
+
 class FailureRecovery:
     def __init__(self):
         self.buffer = []
 
-    def write_log(self, executionResult):
+    def write_log(self, info):
+        # Info type ExecutionResult
         pass
 
-    def recover(self, criteria):
+    def recover(self, criteria: RecoveryCriteria = None):
         pass
 
     def _save_checkpoint(self):
