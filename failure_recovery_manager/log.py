dcce9f659654a7a1070d08e30f6a71e6f3a524a8
19623248Git <13523032@std.stei.itb.ac.id>
2025-11-19 22:22:49 +0700

    fix: :label: add typing

--- failure_recovery_manager/log.py
+++ failure_recovery_manager/log.py
@@ -1,5 +1,23 @@
+import json
+from datetime import datetime
+from typing import Dict
+
+class actiontype(enumerate):
+    start = 0
+    write = 1
+    commit = 2
+    abort = 3
+
 class log:
-    def __init__(self, transaction_id, action, timestamp, old_data = None, new_data = None, table_name : str = None) -> None:
+    def __init__(
+            self, 
+            transaction_id: str | int, 
+            action: actiontype, 
+            timestamp: datetime, 
+            old_data: Dict = None, 
+            new_data: Dict = None, 
+            table_name : str = None
+        ) -> None:
         self.transaction_id = transaction_id
         self.action = action
         self.timestamp = timestamp
@@ -16,10 +34,14 @@ class log:
             f"new_data:{self.new_data}, "
             f"table_name:{self.table_name}, "
         )
-
-
-class actiontype(enumerate):
-    start = 0
-    write = 1
-    commit = 2
-    abort = 3
+    
+    def display(self):
+        data = {
+            "transaction_id":self.transaction_id,
+            "action":self.action,
+            "timestamp":str(self.timestamp),
+            "old_data":self.old_data,
+            "new_data":self.new_data,
+            "table_name":self.table_name
+        }
+        print(json.dumps(data, indent=4))
