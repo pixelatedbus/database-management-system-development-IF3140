84f94b1b218733cc054d8b6b5be98dfee24f1afb
19623248Git <13523032@std.stei.itb.ac.id>
2025-11-19 22:54:20 +0700

    feat: :sparkles: log objects getter

--- failure_recovery_manager/logFile.py
+++ failure_recovery_manager/logFile.py
@@ -1,7 +1,10 @@
-import log
+from log import log
 from pathlib import Path, PosixPath
 from datetime import datetime
 import os
+from typing import List
+import re
+from ast import literal_eval
 
 class logFile:
 
@@ -43,11 +46,68 @@ class logFile:
             with open(self.path) as f:
                 self.logFile_buffer = f.readlines()
         
+        def get_logs(self):
+            if(not  self.logFile_buffer):
+                self.load_file()
+            
+            l_list: List[log] = []
+
+            parse_pattern = re.compile(
+                r"transaction_id:(?P<transaction_id>\d+),\s*"
+                r"action:(?P<action>\d+),\s*"
+                r"timestamp:(?P<timestamp>[\d\s\.:_-]+),\s*"
+                r"old_data:(?P<old_data>\{[^}]*\}),\s*"
+                r"new_data:(?P<new_data>\{[^}]*\}),\s*"
+                r"table_name:(?P<table_name>.*?)[\s,]*\n?$"
+            )   
+            
+            for log_str in self.logFile_buffer:
+
+                match = parse_pattern.match(log_str)
+
+                if not match:
+                    raise ValueError(f"Log string format not recognized: {log_str}")
+
+                data = match.groupdict()
+
+                # convert timestamp into datetime
+                try:
+                    time_format = "%Y-%m-%d_%H-%M-%S"
+                    data['timestamp'] = datetime.strptime(data['timestamp'], time_format)
+                except ValueError as e:
+                    print(f"Error evaluating timestamp in log: {log_str}, Error: {e}")
+                    print(f"Setting timestamp to current time")
+
+                    # IMPORTANT, THIS NEEDS TO MATCH HOW CLASS LOG WORKS
+                    data['timestamp'] = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+
+                # evaluate old/new_data into dictionary
+                try:
+                    data['old_data'] = literal_eval(data['old_data'])
+                    data['new_data'] = literal_eval(data['new_data'])
+                except (ValueError, SyntaxError) as e:
+                    print(f"Error evaluating dictionary data in log: {log_str}. Error: {e}")
+                    print(f"Setting dictionary data empty")
+                    data['old_data'] = {}
+                    data['new_data'] = {}
+                
+                l_list.append(log(
+                    transaction_id=data['transaction_id'],
+                    action=data['action'],
+                    timestamp=data['timestamp'],
+                    new_data=data['new_data'],
+                    old_data=data['old_data'],
+                    table_name=data['table_name']
+                ))
+
+            return l_list
+        
         def write_log(self, logItem: log):
             if(not self.path):
                self.make_new_file()
 
-            with open(self.path, 'w') as f:
+            with open(self.path, 'a') as f:
                 f.write(str(logItem) + '\n')
 
 
