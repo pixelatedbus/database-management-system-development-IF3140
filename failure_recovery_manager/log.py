4aeb8fcfc1203281f9baeed3ae5705428b51cc85
19623248Git <13523032@std.stei.itb.ac.id>
2025-11-19 08:06:07 +0700

    refactor: :recycle: adjusted indentations

--- failure_recovery_manager/log.py
+++ failure_recovery_manager/log.py
@@ -1,15 +1,15 @@
 class log:
-        def __init__(self, transaction_id, action, timestamp, old_data = None, new_data = None, table_name : str = None) -> None:
-                self.transaction_id = transaction_id
-                self.action = action
-                self.timestamp = timestamp
-                self.old_data = old_data
-                self.new_data = new_data
-                self.table_name = table_name
+    def __init__(self, transaction_id, action, timestamp, old_data = None, new_data = None, table_name : str = None) -> None:
+        self.transaction_id = transaction_id
+        self.action = action
+        self.timestamp = timestamp
+        self.old_data = old_data
+        self.new_data = new_data
+        self.table_name = table_name
 
 
 class actiontype(enumerate):
-        start = 0
-        write = 1
-        commit = 2
-        abort = 3
+    start = 0
+    write = 1
+    commit = 2
+    abort = 3
