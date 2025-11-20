2cf0c31c869c0134e2497c571f4797cd735ae87e
Salman Hanif <19623165@std.stei.itb.ac.id>
2025-11-20 14:18:34 +0700

    feat : Algorithm Response

--- concurrency_control_manager/response.py
+++ concurrency_control_manager/response.py
@@ -4,9 +4,12 @@ Response interface for concurrency control operations
 
 from typing import Protocol
 
-
 class Response(Protocol):
     """Response interface for concurrency control decisions"""
+    allowed: bool
+    message: str
+
+class AlgorithmResponse:
     def __init__(self, allowed: bool, message: str):
-        self.allowed: bool = allowed
-        self.message: str = message
+        self.allowed = allowed
+        self.message = message
