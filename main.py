8e47ffdc38fa36d0f567196c095d4dc5b6a281b3
guntarahmbl <guntarahambali3@gmail.com>
2025-11-27 17:18:06 +0700

    test: deadlock scenario

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import sys
 import os
-import datetime
 import threading
 import time
+import logging
+
+logging.basicConfig(
+    level=logging.CRITICAL, # ubah jadi critical kalo mau ga muncul debug, jadi info kalo mau muncul
+    format="[%(levelname)s] %(message)s"
+)
 
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -19,49 +24,45 @@ class ClientThread(threading.Thread):
     def run(self):
         print(f"\n--- Client {self.client_id} Dimulai ---")
         for query in self.scenario:
+
+            time.sleep(0.5) 
+            
             print(f"[C{self.client_id}] Mengirim: {query}")
             result = self.processor.execute_query(query, self.client_id)
             status = "OK" if result.success else "ERROR"
             print(f"[C{self.client_id}] Status: {status} - {result.message}")
-            if not result.success and "aborted" in result.message:
+            
+            if not result.success and "aborted" in result.message.lower():
+                print(f"[C{self.client_id}] Transaksi Dibatalkan (Rollback) oleh Sistem.")
                 break 
-            time.sleep(0.1)
+                
         print(f"--- Client {self.client_id} Selesai ---")
 
-def populate_test_users(processor):
-    print("\n=== Populating test_users ===")
-    processor.execute_query("BEGIN TRANSACTION", client_id=0)
-    processor.execute_query("DROP TABLE test_users", client_id=0)
-    processor.execute_query("CREATE TABLE test_users (id INTEGER, name VARCHAR(50), age INTEGER)", client_id=0)
-    processor.execute_query("INSERT INTO test_users (id, name, age) VALUES (1, 'mifune', 25)", client_id=0)
-    processor.execute_query("INSERT INTO test_users (id, name, age) VALUES (2, 'link', 30)", client_id=0)
-    processor.execute_query("INSERT INTO test_users (id, name, age) VALUES (3, 'samus', 28)", client_id=0)
-    processor.execute_query("INSERT INTO test_users (id, name, age) VALUES (4, 'mario', 35)", client_id=0)
-    processor.execute_query("COMMIT", client_id=0)
-    print("=== test_users ready ===\n")
+
 
 def main():
     
     processor = QueryProcessor()
-    populate_test_users(processor)
-    
-    scenario_1 = [
+
+    scenario_deadlock_1 = [
         "BEGIN TRANSACTION",
-        "SELECT * FROM test_users WHERE id < 3",
+        "UPDATE test_users SET age = 30 WHERE id = 1", 
+        "UPDATE test_products SET price = 5000 WHERE id = 1",
         "COMMIT"
     ]
     
-    scenario_2 = [
+    scenario_deadlock_2 = [
         "BEGIN TRANSACTION",
-        "UPDATE test_users SET age=31 WHERE id=2",
+        "UPDATE test_products SET price = 9000 WHERE id = 1",
+        "UPDATE test_users SET age = 35 WHERE id = 1",
         "COMMIT"
     ]
     
-    client_a = ClientThread(1, processor, scenario_1)
-    client_b = ClientThread(2, processor, scenario_2)
+    client_a = ClientThread(1, processor, scenario_deadlock_1)
+    client_b = ClientThread(2, processor, scenario_deadlock_2)
 
     client_a.start()
-    time.sleep(0.5)  # Let client_a acquire lock first
+    time.sleep(0.5)  # biar client_a dapat kunci dulu
     client_b.start()
 
     client_a.join()
